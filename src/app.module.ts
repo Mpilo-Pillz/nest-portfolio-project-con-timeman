58571492a6e0f0d906483824cea43818b71c6673
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-04 12:22:42 +0200

    adding sanitization for request bodies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { LeaveModule } from './leave/leave.module';
+import { Leave } from './leave/leave.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User, Leave],
       synchronize: true,
     }),
     UsersModule,
+    LeaveModule,
   ],
   controllers: [AppController],
   providers: [AppService],
