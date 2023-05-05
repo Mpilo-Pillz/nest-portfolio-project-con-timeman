79a206a4360fe75b3e6db8eec34873ac5eef55f3
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-05 12:12:45 +0200

    saving new user to db

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+
+    return this.repository.save(user);
+  }
+}
