0fe2383be583dae9f70232b386df43a1fe9019df
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-07 12:12:45 +0200

    removing password form response obect

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   Patch,
   NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -22,6 +24,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.usersService.findOne(parseInt(id));
