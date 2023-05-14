42805616c15ecca9503edfe491691eb6b287f66f
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-14 10:17:50 +0200

    correctly typing user service to get static analysis

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,14 +1,16 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 it('can create an instance of auth service', async () => {
   // Create a fake copy of the users service
 
-  const fakeUsersService = {
+  // auth service only uses find and create
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
     create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password }),
+      Promise.resolve({ id: 1, email, password } as User),
   };
 
   const module = await Test.createTestingModule({
