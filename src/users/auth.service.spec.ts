2e7c20bf66657874952d34e990c4ac6c606c6061
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-14 10:50:20 +0200

    testing if email in use error shows

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,15 +2,17 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
   beforeEach(async () => {
     // Create a fake copy of the users service
 
     // auth service only uses find and create
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       find: () => Promise.resolve([]),
       create: (email: string, password: string) =>
         Promise.resolve({ id: 1, email, password } as User),
@@ -40,5 +42,16 @@ describe('AuthService', () => {
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user sings up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        { id: 1, email: 'test@mail.com', password: 'testUser' } as User,
+      ]);
+
+    await expect(service.signup('test@mail.com', 'testUser')).rejects.toThrow(
+      BadRequestException,
+    );
   });
 });
