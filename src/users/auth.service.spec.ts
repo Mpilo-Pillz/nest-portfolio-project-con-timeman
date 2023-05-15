01bcd812e44192cfd7e476cd76ff19d4dc4c633e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-15 15:34:18 +0200

    adding more intelligent mocking

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -10,12 +10,23 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     // Create a fake copy of the users service
+    const users: User[] = [];
 
     // auth service only uses find and create
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random()) * 999999,
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -73,19 +84,12 @@ describe('AuthService', () => {
   });
 
   it('returns a user if correct password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          email: 'test@mail.com',
-          password:
-            'c910e4ed0b5444e9.ca4aa6df538c04332b38bcfad591cafb036fb048ecda46d138e226b1c6b934bb',
-        } as User,
-      ]);
+    await service.signup('test@mail.com', 'jkghljg');
 
     const user = await service.signin('test@mail.com', 'jkghljg');
     // const user = await service.signin('test@mail.com', 'jkghljg');
     // console.log(user);
 
-    // expect(user).toBeDefined();
+    expect(user).toBeDefined();
   });
 });
