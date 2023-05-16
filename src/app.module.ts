a9b6fc8af5a0525c721370937030b09d74d75bc6
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-16 19:57:57 +0200

    Moving validation pipe and app pipe to appmodule to be used by e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,6 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { LeaveModule } from './leave/leave.module';
 import { Leave } from './leave/leave.entity';
+import { APP_PIPE } from '@nestjs/core';
+// import cookieSession from 'cookie-session';
+
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +23,24 @@ import { Leave } from './leave/leave.entity';
     LeaveModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true, // extra security eg to sanitize
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['mfanakhiti'],
+        }),
+      )
+      .forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['mfanakhiti'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // extra security eg to sanitize
-    }),
-  );
+  // app.use(
+
+  // );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true, // extra security eg to sanitize
+  //   }),
+  // );
   await app.listen(4300);
 }
 bootstrap();
