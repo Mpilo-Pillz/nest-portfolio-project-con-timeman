dbcccf9dbb89bb0af165e8892b258d86ec03673f
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-19 17:15:45 +0200

    adding e2e test for logged in user

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'who@ami.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'pwd123' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie'); // get cookie header not a request I think
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
