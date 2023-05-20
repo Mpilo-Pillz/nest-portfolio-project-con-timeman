2421428578ef158190bfe77d17114e0280a1b90e
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-05-20 17:25:45 +0200

    saving leave day to db

--- src/leave/leave.service.ts
+++ src/leave/leave.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Leave } from './leave.entity';
+import { Repository } from 'typeorm';
+import { AddLeaveDaysDto } from './dtos/add-leave-days.dto';
 
 @Injectable()
 export class LeaveService {
-  addNumberOfLeaveDays(numberOfDays) {
-    console.log(numberOfDays);
+  constructor(@InjectRepository(Leave) private repository: Repository<Leave>) {}
+  addNumberOfLeaveDays(numberOfDaysDto: AddLeaveDaysDto) {
+    const leaveDays = this.repository.create(numberOfDaysDto);
+    return this.repository.save(leaveDays);
   }
 }
