f4bc22cc5d9de36b11b2e0c72349afc366438da2
Mpilo-Pillz <mpilo.dlamini@yahoo.com>
2023-09-26 19:55:59 +0200

    getting session key from env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,11 +43,12 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+  constructor(private configService: ConfigService) {}
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['mfanakhiti'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
